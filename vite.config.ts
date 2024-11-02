0910abc000de39b029c083ec641b91dcbef2e6c2
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-11-03 01:31:24 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allow access from external hosts
+    port: 4173, // You can specify any available port
+    strictPort: true, // Fail if the port is already in use
+  },
+});
